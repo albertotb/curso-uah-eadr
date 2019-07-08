849bf1db81e883230110f17fc3fff13a40db9513
albertotb <albertotb@gmail.com>
2019-07-08 19:09:35 +0200

    use list of input dirs

--- build_slides.R
+++ build_slides.R
@@ -1,16 +1,15 @@
 library(pagedown)
 library(tools)
 
-input_dir  <- "./exercises"
+input_dir  <- c("./src", "./exercises")
 output_dir <- "./slides"
 
 # 1. Take all .Rmd files from `input_dir`
 # 2. Convert them to PDF using `chrome_print()`
 # 3. Move them to `output_dir`
-for (file in list.files(path = input_dir, pattern = ".Rmd", recursive = TRUE)) {
-  input  <- file.path(input_dir, file)
+for (input in list.files(path = input_dir, pattern = ".Rmd", recursive = TRUE, full.names = TRUE)) {
   output <- paste0(file_path_sans_ext(input), ".pdf")
-  target <- file.path(output_dir, paste0(file_path_sans_ext(basename(file)), ".pdf"))
+  target <- file.path(output_dir, paste0(file_path_sans_ext(basename(input)), ".pdf"))
   
   if (!file.exists(target)) {
     pagedown::chrome_print(input)
